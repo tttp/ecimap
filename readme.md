2927f17bc58e4261f2fe195a33f7056148fcfd38
xavier <github@tttp.eu>
2014-11-18 15:29:28 +0100

    add update

---
+++ readme.md
@@ -0,0 +1,2 @@
+
+https://www.policat.org/api/v2/actions/230/signings?segregate=countries
---
+++ update.sh
@@ -0,0 +1,2 @@
+curl -o signatures.json https://www.policat.org/api/v2/actions/230/signings?segregate=countries 
+
